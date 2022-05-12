13fc17c3921d8b9e0a797d12c517f9ea0a04bca4
PritamTuralkar <86599806+PritamTuralkar@users.noreply.github.com>
2022-05-12 17:38:51 +0530

    Add files via upload

---
+++ Boolean.java
@@ -0,0 +1,12 @@
+package Boolean;
+
+public class Boolean {
+	
+	public static void main(String args[])
+	{
+	boolean myBool = true;
+	if(myBool == true)
+	System.out.println("I am using a Boolean data type:"+myBool);}
+
+
+}
---
+++ shortvar.java
@@ -0,0 +1,12 @@
+package shortvar;
+
+public class shortvar {
+
+	public static void main(String[] args) {
+		short myShort = 6000;
+		System.out.println("myShort: " + myShort);
+
+
+	}
+
+}
